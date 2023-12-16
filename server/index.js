5b351ac348304ff04d999935fead5f84bdf96eb3
kayesAshfaq <asfak7074@gmail.com>
2023-12-16 23:13:56 +0600

    server config add demo get api added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
 console.log("Hello, Node!");
+
+// import express
+const express = require("express");
+const app = express();
+
+// init server info
+const PORT = 3000;
+const IP = "0.0.0.0";
+
+// creating a server
+app.listen(PORT, function () {
+  console.log(`connection established at port ${PORT}`);
+});
+
+// initial api
+app.get("/", function (req, res) {
+  res.send(`hello from PORT : ${PORT}`);
+});
+
+// a get api
+app.get("/hello-world", function (req, res) {
+  res.json({ hi: "hello world" });
+});
--- server/readme.md
+++ server/readme.md
@@ -21,3 +21,12 @@ add : node i or install package_name
 to add multiple packages at the same time : node i package_1 package_2 ...
 
 to add with specific version : node i package_name@version_num
+
+to add dev dependency : npm i package_name --save-dev
+
+to add nodemon dev script : inside package.json file add
+    "dev": "nodemon ./index.js"
+now run using : npm run dev
+
+to activate 'npm start' command add this in script
+"start": "node ./index.js"
