225d9d7f1cddda7fb9d87c36b6681ee4814bea31
kayesAshfaq <asfak7074@gmail.com>
2024-04-22 00:48:32 +0600

    Add new product API, get all products API, and delete product API implemented

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,6 +5,7 @@ const admin = require("../middlewares/admin");
 
 const adminRouter = express.Router();
 
+// add new products
 adminRouter.post("/admin/add-product", admin, async (req, res) => {
   try {
     const { name, description, images, quantity, price, category } = req.body;
@@ -25,6 +26,41 @@ adminRouter.post("/admin/add-product", admin, async (req, res) => {
     delete product.__v;
 
     res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// get all products
+adminRouter.get("/admin/all-products", admin, async (req, res) => {
+  try {
+    let products = await Product.find({});
+
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// delete a product by id
+adminRouter.delete("/admin/delete-product/:id", admin, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json({
+      message: "Product deleted successfully",
+      product: {
+        id: product.id,
+        name: product.name,
+        category: product.category,
+      },
+    });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
